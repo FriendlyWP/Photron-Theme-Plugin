2f0fe2c3e3e872256457e98d64ed572f8667ff2d
FriendlyWP <michelle@friendlywebconsulting.com>
2015-01-30 10:20:31 -0600

    Updates to look-and-feel

--- post-formats/format.php
+++ post-formats/format.php
@@ -20,10 +20,6 @@
 
                   <h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
 
-                  <p class="byline vcard">
-                    <?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-                  </p>
-
                 </header> <?php // end article header ?>
 
                 <section class="entry-content cf" itemprop="articleBody">
@@ -52,14 +48,18 @@
                   ?>
                 </section> <?php // end article section ?>
 
-                <footer class="article-footer">
+               <!--  <footer class="article-footer">
 
                   <?php printf( __( 'Filed under: %1$s', 'bonestheme' ), get_the_category_list(', ') ); ?>
 
                   <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 
-                </footer> <?php // end article footer ?>
+                  <p class="byline vcard">
+                    <?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
+                  </p>
 
-                <?php comments_template(); ?>
+                </footer> <?php // end article footer ?> -->
+
+                <?php // comments_template(); ?>
 
               </article> <?php // end article ?>
